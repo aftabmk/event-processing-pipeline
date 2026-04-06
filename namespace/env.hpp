44f4fa75f4fcffd544876b311c1e05e72b36337f
Aftab mohammed k <87110394+aftabmk@users.noreply.github.com>
2026-04-06 19:49:29 +0530

    modified:   command.txt 	modified:   namespace/env.hpp
    	updated command and env

--- namespace/env.hpp
+++ namespace/env.hpp
@@ -4,7 +4,7 @@
 namespace ENV {
     inline const char* URL = std::getenv("URL");
     inline const char* KEY = std::getenv("KEY");
-    inline const char* isLambda = std::getenv("ENV"); 
+    inline const char* IS_LAMBDA = std::getenv("ENV"); 
     inline const char* EXCHANGE_1 = std::getenv("EXCHANGE_1");
     inline const char* EXCHANGE_2 = std::getenv("EXCHANGE_2");
 }
